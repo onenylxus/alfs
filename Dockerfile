89d12a1ff50e8947d560eea9b839207c468f430f
Nicholas <onenylxus@gmail.com>
2025-03-12 23:02:35 +0800

    Add grep.sh

--- Dockerfile
+++ Dockerfile
@@ -52,3 +52,4 @@ RUN $LFS_SH/ch6/diffutils.sh
 RUN $LFS_SH/ch6/file.sh
 RUN $LFS_SH/ch6/findutils.sh
 RUN $LFS_SH/ch6/gawk.sh
+RUN $LFS_SH/ch6/grep.sh
---
+++ lfs/ch6/grep.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+echo "Building grep..."
+echo "Approximate build time: 0.1 SBU"
+echo "Required disk space: 27 MB"
+
+tar -xf /sources/grep-*.tar.xz -C /tmp/
+mv /tmp/grep-* /tmp/grep
+
+pushd /tmp/grep > /dev/null
+
+time
+{
+  ./configure       \
+    --prefix=/usr   \
+    --host=$LFS_TGT \
+    --build=$(./build-aux/config.guess)
+
+  make
+  make DESTDIR=$LFS install
+}
+
+popd > /dev/null
+rm -rf /tmp/grep
